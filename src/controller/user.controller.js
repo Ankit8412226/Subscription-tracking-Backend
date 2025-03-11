42b8e30edb93d17a6f324c2e7be3df8468969b70
Ankit8412226 <125825310+Ankit8412226@users.noreply.github.com>
2025-03-12 04:29:49 +0530

    Login added

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,7 @@
 import { User } from "../models/index.js";
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.SECRET_KEY;
 
 export const createUser = async (req, res) => {
   const { email } = req.body;
@@ -15,6 +18,38 @@ export const createUser = async (req, res) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (err) {
     console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const isMatch = await existingUser.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    const accessToken = jwt.sign({ userId: existingUser._id }, JWT_SECRET, {
+      expiresIn: "1m",
+    });
+    const refreshToken = jwt.sign({ email: email }, JWT_SECRET, {
+      expiresIn: "3m",
+    });
+
+    res.status(200).json({
+      message: "Login Successful",
+      data: existingUser,
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    console.log(err);
     res.status(500).json({ error: "Server error" });
   }
 };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,22 +1,44 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlegth: 10,
+    },
+    refreshToken: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-    maxlegth: 10,
-  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
+
+// Method to compare password
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model("User", UserSchema);
 
